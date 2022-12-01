ed28ea32f32f90870d7123ce1c6f4d5d6a2ec756
Teddy <teddy@teddy.email>
2022-12-01 23:43:13 +0100

    made authentication user specific to access and modify user data

---
+++ SpotifyAPI/CallBackListener.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SpotifyAPI;
+
+public class CallBackListener
+{
+    private HttpListener _httpListener = new HttpListener();
+
+    public void StartListener()
+    {
+        _httpListener.Prefixes.Add("http://localhost:5000/callback/");
+        _httpListener.Start();
+        var _responseThread = new Thread(ResponseThread);
+        _responseThread.Start();
+    }
+    
+    private void ResponseThread()
+    {
+        HttpListenerContext context = _httpListener.GetContext();
+        var respURL = context.Request.Url.ToString();
+
+        byte[] _responseArray = 
+            """
+                <html>
+                    <head>
+                        <title>
+                            Authentication Successful
+                        </title>
+                    </head>
+                    <body>
+                        The Spotify Authentication was Successful. You can now close this page.
+                    </body>
+                </html>
+            """u8.ToArray();
+        context.Response.OutputStream.Write(_responseArray, 0, _responseArray.Length);
+        context.Response.KeepAlive = false;
+        context.Response.Close();
+
+        var code = Regex.Match(respURL, "(?<=code=).*").ToString();
+        Connection.GetCallback(code);
+    }
+}
--- SpotifyAPI/Connection.cs
+++ SpotifyAPI/Connection.cs
@@ -1,21 +1,87 @@
-﻿using SpotifyAPI.Web;
+﻿using Microsoft.Maui.ApplicationModel;
+using SpotifyAPI.Web;
 
 namespace SpotifyAPI;
 
-public class Connection
+public static class Connection
 {
-    public SpotifyClient SpotifyClient { get; }
-    
-    public Connection()
+    private static string _verifier;
+    private static PKCETokenResponse _initialResponse;
+    // loaded from local file for development
+    private static string _clientId = File.ReadAllLines("./SpotifyKeys/clientId.txt")[0];
+
+    public static SpotifyClient SpotifyClient { get; private set; }
+
+    public static void Authenticate()
     {
-        // loading api keys from files for development
-        string _clientId = File.ReadAllLines("./SpotifyKeys/clientId.txt")[0];
-        string _clientSecret = File.ReadAllLines("./SpotifyKeys/clientSecret.txt")[0];
+        if (SpotifyClient == null)
+        {
+            NewAuthentication();
+        }
+        else
+        {
+            RefreshAuthentication();
+        }
+    }
+    
+    private static async void NewAuthentication()
+    {
+        // Generates a secure random verifier of length 100 and its challenge
+        (_verifier,string challenge) = PKCEUtil.GenerateCodes();
         
-        var config = SpotifyClientConfig
-            .CreateDefault()
-            .WithAuthenticator(new ClientCredentialsAuthenticator(_clientId, _clientSecret));
+        var loginRequest = new LoginRequest(
+            new Uri("http://localhost:5000/callback"),
+            _clientId,
+            LoginRequest.ResponseType.Code
+        )
+        {
+            CodeChallengeMethod = "S256",
+            CodeChallenge = challenge,
+            Scope = new[] {
+                Scopes.Streaming, 
+                Scopes.AppRemoteControl, 
+                Scopes.PlaylistModifyPrivate, 
+                Scopes.PlaylistModifyPublic, 
+                Scopes.PlaylistReadCollaborative, 
+                Scopes.PlaylistReadPrivate, 
+                Scopes.UgcImageUpload, 
+                Scopes.UserFollowModify, 
+                Scopes.UserFollowRead, 
+                Scopes.UserLibraryModify, 
+                Scopes.UserLibraryRead, 
+                Scopes.UserReadEmail, 
+                Scopes.UserReadPrivate, 
+                Scopes.UserTopRead, 
+                Scopes.UserModifyPlaybackState, 
+                Scopes.UserReadCurrentlyPlaying, 
+                Scopes.UserReadPlaybackPosition, 
+                Scopes.UserReadPlaybackState, 
+                Scopes.UserReadRecentlyPlayed
+            }
+        };
+        // start webserver for callback
+        var callBackListener = new CallBackListener();
+        callBackListener.StartListener();
         
-        SpotifyClient = new SpotifyClient(config);
+        var uri = loginRequest.ToUri();
+        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private static async void RefreshAuthentication()
+    {
+        var newResponse = await new OAuthClient().RequestToken(
+            new PKCETokenRefreshRequest(_clientId, _initialResponse.RefreshToken)
+        );
+
+        SpotifyClient = new SpotifyClient(newResponse.AccessToken);
+    }
+
+    internal static async Task GetCallback(string code)
+    {
+        _initialResponse = await new OAuthClient().RequestToken(
+            new PKCETokenRequest(_clientId, code, new Uri("http://localhost:5000/callback"), _verifier)
+        );
+
+        SpotifyClient = new SpotifyClient(_initialResponse.AccessToken);
     }
 }
