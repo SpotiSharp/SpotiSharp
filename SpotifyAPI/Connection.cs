10d9a9dd6f72595fc6b79799d4b93176b01c76a1
Teddy <teddy@teddy.email>
2022-12-01 18:01:13 +0100

    connection now creates a spotify api connection

--- SpotifyAPI/Connection.cs
+++ SpotifyAPI/Connection.cs
@@ -1,5 +1,21 @@
-﻿namespace SpotifyAPI;
+﻿using SpotifyAPI.Web;
+
+namespace SpotifyAPI;
 
 public class Connection
 {
+    public SpotifyClient SpotifyClient { get; }
+    
+    public Connection()
+    {
+        // loading api keys from files for development
+        string _clientId = File.ReadAllLines("./SpotifyKeys/clientId.txt")[0];
+        string _clientSecret = File.ReadAllLines("./SpotifyKeys/clientSecret.txt")[0];
+        
+        var config = SpotifyClientConfig
+            .CreateDefault()
+            .WithAuthenticator(new ClientCredentialsAuthenticator(_clientId, _clientSecret));
+        
+        SpotifyClient = new SpotifyClient(config);
+    }
 }
