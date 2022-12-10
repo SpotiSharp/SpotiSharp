1a820baa67fe9044ce872b1de43ec7a15b418a01
Teddy <teddy@teddy.email>
2022-12-10 16:08:02 +0100

    add new method to Playlist api class

--- SpotifyAPI/PlayList.cs
+++ SpotifyAPI/PlayList.cs
@@ -19,4 +19,12 @@ public static class PlayList
         FullPlaylist playlist = GetPlaylistById(playlistId);
         return playlist.Tracks ??= new Paging<PlaylistTrack<IPlayableItem>>();
     }
+
+    public static List<string> GetTrackIdsByPlaylistId(string playlistId)
+    {
+        Paging<PlaylistTrack<IPlayableItem>> songs = GetTracksByPlaylistId(playlistId);
+        return (from song in songs.Items 
+            let songAsTrack = song.Track as FullTrack 
+            select songAsTrack.Uri).ToList();
+    }
 }
