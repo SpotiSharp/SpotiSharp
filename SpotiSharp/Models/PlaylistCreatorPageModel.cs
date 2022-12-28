0cd980abcbd45a27e864c643388904807dc69555
Teddy <teddy@teddy.email>
2022-12-28 07:05:04 +0100

    add PlaylistCreatorPageModel to hold and coordinate the current playlist that is created

---
+++ SpotiSharp/Models/PlaylistCreatorPageModel.cs
@@ -0,0 +1,70 @@
+﻿using SpotifyAPI;
+using SpotifyAPI.Web;
+using SpotiSharp.Interfaces;
+
+namespace SpotiSharp.Models;
+
+public delegate void SongsChange();
+public static class PlaylistCreatorPageModel
+{
+    private static List<FullTrack> _unfilteredSongs = new List<FullTrack>();
+
+    private static List<FullTrack> UnfilteredSongs
+    {
+        get
+        {
+            return _unfilteredSongs;
+        }
+        set
+        {
+            OnSongListChange?.Invoke(); 
+            _unfilteredSongs = value;
+        }
+    }
+
+    public static List<IFilterViewModel> Filters { get; set; } = new List<IFilterViewModel>();
+
+    public static event SongsChange OnSongListChange;
+
+    public static void AddSong(string songId)
+    {
+        FullTrack? song = APICaller.Instance?.GetTrackById(songId);
+        var tmpSongs = UnfilteredSongs;
+        if (song != null) tmpSongs.Add(song);
+        UnfilteredSongs = tmpSongs;
+    }
+    
+    public static void AddSongsFromPlaylist(string playlistId)
+    {
+        Paging<PlaylistTrack<IPlayableItem>>? songs = APICaller.Instance?.GetTracksByPlaylistId(playlistId);
+        var tmpSongs = UnfilteredSongs;
+        if (songs?.Items == null) return;
+        foreach (var playable in songs.Items)
+        {
+            if (playable.Track is FullTrack song)
+            {
+                tmpSongs.Add(song);
+            }
+        }
+
+        UnfilteredSongs = tmpSongs;
+    }
+
+    public static void RemoveSongByIndex(int index)
+    {
+        var tmpSongs = UnfilteredSongs;
+        tmpSongs.RemoveAt(index);
+        UnfilteredSongs = tmpSongs;
+    }
+    
+    public static List<FullTrack> GetFilteredSongs()
+    {
+        var resultSongs = UnfilteredSongs;
+        foreach (var filter in Filters)
+        {
+            resultSongs = filter.FilterSongs(resultSongs);
+        }
+
+        return resultSongs;
+    }
+}
--- SpotiSharp/ViewModels/PlaylistCreatorPageViewModel.cs
+++ SpotiSharp/ViewModels/PlaylistCreatorPageViewModel.cs
@@ -1,6 +1,48 @@
-﻿namespace SpotiSharp.ViewModels;
+﻿using System.Windows.Input;
+using SpotifyAPI;
+using SpotiSharp.Models;
 
-public class PlaylistCreatorPageViewModel
+namespace SpotiSharp.ViewModels;
+
+public class PlaylistCreatorPageViewModel : BaseViewModel
 {
+    private string _selectedPlaylistNameId;
+
+    public string SelectedPlaylistNameId
+    {
+        get { return _selectedPlaylistNameId; }
+        set { SetProperty(ref _selectedPlaylistNameId, value); }
+    }
+
+    private List<string> _playlistNamesIds;
+
+    public List<string> PlaylistNamesIds
+    {
+        get { return _playlistNamesIds; }
+        set { SetProperty(ref _playlistNamesIds, value); }
+    }
+
+    public PlaylistCreatorPageViewModel()
+    {
+        AddSongsFromPlaylist = new Command(AddSongsFromPlaylistHandler);
+        AddFilter = new Command(AddFilterHandler);
+
+        var playlistListModel = new PlaylistListModel();
+        // TODO: what if playlist load fails first time (may need to implement some sort of retry)
+        PlaylistNamesIds = playlistListModel.PlayLists.Select(p => $"{p.PlayListTitle}\n{p.PlayListId}").ToList();
+    }
     
+    private void AddSongsFromPlaylistHandler()
+    {
+        if (SelectedPlaylistNameId != null) PlaylistCreatorPageModel.AddSongsFromPlaylist(SelectedPlaylistNameId.Split("\n")[1]);
+    }
+    
+    private void AddFilterHandler()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ICommand AddSongsFromPlaylist { private set; get; }
+    public ICommand AddFilter { private set; get; }
+
 }
