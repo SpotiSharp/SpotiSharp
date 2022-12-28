ba3531eef05c9dbaf08a3365ab3b14987510f856
Teddy <teddy@teddy.email>
2022-12-28 07:03:13 +0100

    add PlaylistCreationSonglistView to display the currently selected songs

---
+++ SpotiSharp/ViewModels/PlaylistCreationSonglistViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+using SpotiSharp.Models;
+
+namespace SpotiSharp.ViewModels;
+
+public class PlaylistCreationSonglistViewModel : BaseViewModel
+{
+    private List<Song> _songs = new List<Song>();
+
+    public List<Song> Songs
+    {
+        get { return _songs; }
+        set { SetProperty(ref _songs, value); }
+    }
+
+    public PlaylistCreationSonglistViewModel()
+    {
+        RemoveSong = new Command(RemoveSongHandler);
+        
+        PlaylistCreatorPageModel.OnSongListChange += RefreshSongs;
+    }
+
+    private void RefreshSongs()
+    {
+        Songs = (from fullTrack in PlaylistCreatorPageModel.GetFilteredSongs() select new Song(fullTrack)).ToList();
+    }
+    
+    private void RemoveSongHandler(object obj)
+    {
+        var index = 0;
+        PlaylistCreatorPageModel.RemoveSongByIndex(index);
+    }
+
+    public ICommand RemoveSong { private set; get; }
+}
---
+++ SpotiSharp/Views/PlaylistCreationSonglistView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotiSharp.ViewModels;
+
+namespace SpotiSharp.Views;
+
+public partial class PlaylistCreationSonglistView : ContentView
+{
+    public PlaylistCreationSonglistView()
+    {
+        InitializeComponent();
+        BindingContext = new PlaylistCreationSonglistViewModel();
+    }
+}
