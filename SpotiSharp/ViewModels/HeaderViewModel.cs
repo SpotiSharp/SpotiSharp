e5e423c4ef132ac1475e0b4c5cf1ccd589b2b936
Teddy <teddy@teddy.email>
2022-12-30 18:30:22 +0100

    add custom header navigation

---
+++ SpotiSharp/ViewModels/HeaderViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+
+namespace SpotiSharp.ViewModels;
+
+public class HeaderViewModel : BaseViewModel
+{
+    public HeaderViewModel()
+    {
+        HomeClicked = new Command(SwitchToHome);
+        PlaylistCreationClicked = new Command(SwitchToPlaylistCreation);
+        ViewPlaylistsClicked = new Command(SwitchToViewPlaylists);
+        AuthenticationClicked = new Command(SwitchToAuthentication);
+    }
+
+    private async void SwitchToHome()
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+
+    private async void SwitchToPlaylistCreation()
+    {
+        await Shell.Current.GoToAsync("//PlaylistCreatorPage");
+    }
+
+    private async void SwitchToViewPlaylists()
+    {
+        await Shell.Current.GoToAsync("//ManagePlayListsPage");
+    }
+
+    private async void SwitchToAuthentication()
+    {
+        await Shell.Current.GoToAsync("//AuthenticationPage");
+    }
+
+    public ICommand HomeClicked { private set; get; }
+    public ICommand PlaylistCreationClicked { private set; get; }
+    public ICommand ViewPlaylistsClicked { private set; get; }
+    public ICommand AuthenticationClicked { private set; get; }
+}
---
+++ SpotiSharp/Views/HeaderView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using SpotiSharp.ViewModels;
+
+namespace SpotiSharp.Views;
+
+public partial class HeaderView : ContentView
+{
+    public HeaderView()
+    {
+        InitializeComponent();
+        BindingContext = new HeaderViewModel();
+    }
+}
