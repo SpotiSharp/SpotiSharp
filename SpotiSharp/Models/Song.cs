87b37ea8da2e3b8085e312797d1b1e6a46c971bf
Teddy <teddy@teddy.email>
2022-12-30 10:37:30 +0100

    created SongEditable for display in filtered song list

--- SpotiSharp/Models/Song.cs
+++ SpotiSharp/Models/Song.cs
@@ -4,12 +4,13 @@ namespace SpotiSharp.Models;
 
 public class Song
 {
-    public string SongId { get; private set; }
-    public string SongImageURL { get; private set; }
-    public string SongTitle { get; private set; }
-    public string SongArtists { get; private set; }
-    
+    public string SongId { get; set; }
+    public string SongImageURL { get; set; }
+    public string SongTitle { get; set; }
+    public string SongArtists { get; set; }
     public string PartOfPlayListWithId { get; private set; }
+
+    public Song() { }
     
     public Song(string songId, string songImageURL, string songTitle, string songArtists, string partOfPlayListWithId)
     {
@@ -18,13 +19,5 @@ public class Song
         SongTitle = songTitle;
         SongArtists = songArtists;
         PartOfPlayListWithId = partOfPlayListWithId;
-    }
-
-    public Song(FullTrack fullTrack)
-    {
-        SongId = fullTrack.Id;
-        SongImageURL = fullTrack.Album.Images[0].Url;
-        SongTitle = fullTrack.Name;
-        SongArtists = string.Join(", ", fullTrack.Artists);
     }
 }
---
+++ SpotiSharp/Models/SongEditable.cs
@@ -0,0 +1,17 @@
+﻿using SpotifyAPI.Web;
+
+namespace SpotiSharp.Models;
+
+public class SongEditable : Song
+{
+    public int Index { get; private set; }
+    
+    public SongEditable(int index, FullTrack fullTrack)
+    {
+        Index = index;
+        SongId = fullTrack.Id;
+        SongImageURL = fullTrack.Album.Images[0].Url;
+        SongTitle = fullTrack.Name;
+        SongArtists = string.Join(", ", fullTrack.Artists);
+    }
+}
