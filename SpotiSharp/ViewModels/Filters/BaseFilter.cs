e5aa90e54c8c7f944378ba6455b5555d547bcd66
Teddy <teddy@teddy.email>
2022-12-30 17:00:32 +0100

    added base filter for shared event invoke

---
+++ SpotiSharp/ViewModels/Filters/BaseFilter.cs
@@ -0,0 +1,16 @@
+﻿using SpotiSharp.Models;
+
+namespace SpotiSharp.ViewModels.Filters;
+
+public delegate void FilterRemove(int index);
+
+public class BaseFilter : BaseViewModel
+{
+    public static event FilterRemove OnFilterRemove;
+
+    internal void InvokeEvent(int index)
+    {
+        OnFilterRemove?.Invoke(index);
+        PlaylistCreatorPageModel.Filters.RemoveAt(index);
+    }
+}
