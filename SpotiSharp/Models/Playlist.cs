fec17d876b2f8a4c4a23ec3936e1a05a6e0baa6e
Teddy <teddy@teddy.email>
2022-12-30 10:29:59 +0100

    extra text is now added in xaml

--- SpotiSharp/Models/Playlist.cs
+++ SpotiSharp/Models/Playlist.cs
@@ -7,23 +7,22 @@ public class Playlist
     public string PlayListId { get; private set; }
     public string PlayListImageURL { get; private set; }
     public string PlayListTitle { get; private set; }
-    public string SongAmount { get; private set; }
+    public int SongAmount { get; private set; }
     
     public Playlist(string playListId, string playListImageUrl, string playListTitle, int? songAmount)
     {
         PlayListId = playListId;
         PlayListImageURL = playListImageUrl;
         PlayListTitle = playListTitle;
-        SongAmount = $"{songAmount} Songs";
+        SongAmount = songAmount ?? 0;
     }
 
     public override bool Equals(object? obj)
     {
         if (obj is not Playlist playlistComp) return false;
-        if (playlistComp.PlayListId.Equals(PlayListId) &&
-            playlistComp.PlayListImageURL.Equals(PlayListImageURL) &&
-            playlistComp.PlayListTitle.Equals(PlayListTitle) &&
-            playlistComp.SongAmount.Equals(SongAmount)) return true;
-        return false;
+        return playlistComp.PlayListId.Equals(PlayListId) &&
+               playlistComp.PlayListImageURL.Equals(PlayListImageURL) &&
+               playlistComp.PlayListTitle.Equals(PlayListTitle) &&
+               playlistComp.SongAmount.Equals(SongAmount);
     }
 }
