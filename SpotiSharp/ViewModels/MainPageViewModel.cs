ccdd8603fe6e2b954fe3ea952fddcdeaccea23e9
Teddy <teddy@teddy.email>
2022-12-31 22:34:50 +0100

    added hint for users to sign in

--- SpotiSharp/ViewModels/MainPageViewModel.cs
+++ SpotiSharp/ViewModels/MainPageViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace SpotiSharp.ViewModels;
+﻿using SpotifyAPI;
+
+namespace SpotiSharp.ViewModels;
 
 public class MainPageViewModel : BaseViewModel
 {
-    
+    private bool _isUserIsNotAuthenticated = Authentication.SpotifyClient == null;
+
+    public bool IsUserIsNotAuthenticated
+    {
+        get { return _isUserIsNotAuthenticated; }
+        set { SetProperty(ref _isUserIsNotAuthenticated, value); }
+    }
+
+    internal override void OnAppearing()
+    {
+        IsUserIsNotAuthenticated = Authentication.SpotifyClient == null;
+    }
 }
