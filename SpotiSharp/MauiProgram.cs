b91bf14ddc67c8d4d61736b547ef50fc8de4d548
Teddy <teddy@teddy.email>
2022-12-31 19:02:30 +0100

    add syncfusion key default

--- SpotiSharp/MauiProgram.cs
+++ SpotiSharp/MauiProgram.cs
@@ -15,6 +15,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+		Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("YOUR LICENSE KEY");
 		var uiRefresherThread = new Thread(UiLoop.Instance.Loop);
 		uiRefresherThread.Start();
 #if DEBUG
