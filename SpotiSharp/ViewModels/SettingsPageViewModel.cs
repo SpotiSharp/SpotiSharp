f81851a55a73ffe9b03bf7ec3dd95ef6c6dcdce6
Teddy <teddy@teddy.email>
2023-01-08 09:42:16 +0100

    add settings page

---
+++ SpotiSharp/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Input;
+using SpotiSharpBackend;
+
+namespace SpotiSharp.ViewModels;
+
+public class SettingsPageViewModel : BaseViewModel
+{
+    private bool _isUsingCollaborationHost;
+
+    public bool IsUsingCollaborationHost
+    {
+        get { return _isUsingCollaborationHost; }
+        set { SetProperty(ref _isUsingCollaborationHost, value); }
+    }
+    
+    private string _collaborationHostAddress;
+
+    public string CollaborationHostAddress
+    {
+        get { return _collaborationHostAddress; }
+        set { SetProperty(ref _collaborationHostAddress, value); }
+    }
+    
+    private string _collaborationSession;
+
+    public string CollaborationSession
+    {
+        get { return _collaborationSession; }
+        set { SetProperty(ref _collaborationSession, value); }
+    }
+
+    public SettingsPageViewModel()
+    {
+        ApplySettings = new Command(() =>
+        {
+            StorageHandler.IsUsingCollaborationHost = IsUsingCollaborationHost;
+            StorageHandler.CollaborationHostAddress = CollaborationHostAddress;
+            StorageHandler.CollaborationSession = CollaborationSession;
+        });
+    }
+    
+    public ICommand ApplySettings { private set; get; }
+}
---
+++ SpotiSharp/Views/SettingsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using SpotiSharp.ViewModels;
+
+namespace SpotiSharp.Views;
+
+public partial class SettingsPage : BasePage
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+        BindingContext = new SettingsPageViewModel();
+    }
+}
